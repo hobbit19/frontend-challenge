8136957097f2050597fb5c55d84852566c1080aa
Jyotirmoy Barman <jewelbarman998@gmail.com>
2020-07-10 21:23:43 +0530

    Updates

---
+++ precache-manifest.725480592ced292b3b4d2d698412f85f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891409c5524e1d22ae2cf3f0ac922772",
+    "url": "/frontend-challenge/index.html"
+  },
+  {
+    "revision": "25729f530ac5495a8f86",
+    "url": "/frontend-challenge/static/css/main.dd5999c9.chunk.css"
+  },
+  {
+    "revision": "56f98fc9b9530c77df3a",
+    "url": "/frontend-challenge/static/js/2.75978a33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend-challenge/static/js/2.75978a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25729f530ac5495a8f86",
+    "url": "/frontend-challenge/static/js/main.e73e691d.chunk.js"
+  },
+  {
+    "revision": "a6301b192720c2fe64bd",
+    "url": "/frontend-challenge/static/js/runtime-main.516e6923.js"
+  }
+]);
